31b92e73a452c5e8acddec91184ddc7f055c0c72
Tagir Murtazin <tr.murtazin@yandex.ru>
2023-12-11 14:54:08 +0300

    Доработать Homework11 10 (переполнение byte) Добавлены Homework14, Homework15, Homework17, Homework20

---
+++ src/main/java/ru/murtazin/homeworks/homework20/task54/ExceptionTaskDivide.java
@@ -0,0 +1,24 @@
+package ru.murtazin.homeworks.homework20.task54;
+
+import java.util.Arrays;
+
+/**
+ * ДЗ (54)
+ * Реализуйте метод, который принимает чётное количество целочисленных элементов и попарно делит их друг на друга.
+ * Если в качестве делителя встречается ноль, то нужно отловить ArithmeticException и записать в качестве результата 0.
+ * Если общее количество элементов не является чётным, метод должен бросить IllegalArgumentException.
+ * Метод должен вернуть массив с результатами деления (использовать обычное деление).
+ */
+public class ExceptionTaskDivide {
+    private ExceptionTaskDivide(){}
+
+    public static int[] divide(int... numbers) {
+//TODO
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Делим попарно 4, 2, 4, 1: " + Arrays.toString(divide(4, 2, 4, 1)));
+        System.out.println("Делим попарно 0, 2, 0, 0: " + Arrays.toString(divide(0, 2, 0, 0)));
+    }
+}
---
+++ src/main/java/ru/murtazin/homeworks/homework20/task56/ExceptionTaskCustomException.java
@@ -0,0 +1,32 @@
+package ru.murtazin.homeworks.homework20.task56;
+
+/**
+ * ДЗ (56)
+ * Дан метод, который принимает два целочисленных значения и выводит разницу их абсолютного значения |left - right| -
+ * однако в случае если left < right метод возвращает CustomException - реализуйте класс данного исключения.
+ * В качестве сообщения для исключения метод должен использовать строку 'my custom exception'
+ */
+public class ExceptionTaskCustomException {
+    public static int simpleAbs(int left, int right) throws CustomException {
+        if (left < right) {
+            //throw new CustomException("my custom exception");
+        }
+        return left - right;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("No exception: " + simpleAbs(3, 2));
+        try {
+            System.out.println("Exception: " + simpleAbs(1, 2));
+            System.out.println("Исключение не брошено, ошибка!");
+        } catch (CustomException e) {
+            System.out.println("Успешно брошено исключение.");
+        }
+    }
+}
+
+class CustomException extends Exception {
+
+//TODO
+}
+
